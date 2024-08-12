6f829fb413975f238422aa7c40605fa8b8f0ba5d
[Aun Ali] <[rajaaunalikhan@gmail.com]>
2024-08-12 12:46:59 +0500

    make a seperate js file to add the json content and then map it in homepage

--- app/page.jsx
+++ app/page.jsx
@@ -2,26 +2,28 @@ import fs from "fs";
 import path from "path";
 import styles from "./page.module.css";
 import HomepageCSR from "./components/clientPagesComponents/HomepageCSR";
-
+import maindata from "./list";
 export default async function Home() {
-  const filePath = path.join(process.cwd(), "app", "list.json");
+  // const filePath = path.join(process.cwd(), "app", "list.json");
 
-  if (!fs.existsSync(filePath)) {
-    console.error(`File not found: ${filePath}`);
-    return (
-      <main className={styles.main}>
-        <p>File not found: {filePath}</p>
-      </main>
-    );
-  }
+  // if (!fs.existsSync(filePath)) {
+  //   console.error(`File not found: ${filePath}`);
+  //   return (
+  //     <main className={styles.main}>
+  //       <p>File not found: {filePath}</p>
+  //     </main>
+  //   );
+  // }
 
-  const jsonData = fs.readFileSync(filePath, "utf8");
-  const data = JSON.parse(jsonData);
+  // const jsonData = fs.readFileSync(filePath, "utf8");
+  // const data = JSON.parse(jsonData);
+
+  // const data = JSON.parse(maindata);
 
   return (
     <>
       <main className={styles.main}>
-        {data.map((d) => (
+        {maindata.map((d) => (
           <>
             <HomepageCSR
               image={d.src}
