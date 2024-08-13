a862c6dbfe5350e40b5a0c9ad5abc1351bcca3b2
[Aun Ali] <[rajaaunalikhan@gmail.com]>
2024-08-13 12:03:06 +0500

    change footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,35 +1,49 @@
+"use client";
+import { useState, useEffect } from "react";
 import React from "react";
 import styles from "./clientside.module.css";
+
 export const Footer = () => {
   return (
     <div className={styles.mainfooter}>
       <div className={styles.footer}>
-        <div className={styles.footinfo}>
-          <div className={styles.subhead}>INFORMATION</div>
-          <div>+923171777008</div>
-          <div>Info@krosskulture.com</div>
-          <div>contact us</div>
-          <div>About us</div>
-          <div>Blog</div>
-          <div>Track order</div>
-        </div>
+        <details className={styles.accordion} open>
+          <summary className={styles.subhead}>
+            INFORMATION <span className={styles.span}>&#x3e;</span>
+          </summary>
+          <div className={styles.life}>
+            <div>+923171777008</div>
+            <div>Info@krosskulture.com</div>
+            <div>Contact us</div>
+            <div>About us</div>
+            <div>Blog</div>
+            <div>Track order</div>
+          </div>
+        </details>
 
-        <div className={styles.footsubscribe}>
-          <div className={styles.subhead}>SUBSCRIBE</div>
-          <p>GET THE LATEST TREND FIRST</p>
-          <p>Join Kross Kulture Premium family to know about</p>
-          <p>latest updates and discounts.</p>
-        </div>
+        <details className={styles.accordion} open>
+          <summary className={styles.subhead}>
+            SUBSCRIBE <span className={styles.span}>&#x3e;</span>
+          </summary>
+          <div className={styles.life}>
+            <p>GET THE LATEST TREND FIRST</p>
+            <p>Join Kross Kulture Premium family to know about</p>
+            <p>latest updates and discounts.</p>
+          </div>
+        </details>
 
-        <div className={styles.custome}>
-          <div className={styles.subhead}>CUSTOMER CARE</div>
-
-          <div>Size Guide</div>
-          <div>Shipping and Exchange</div>
-          <div>Store Locator</div>
-          <div>Privacy policy</div>
-          <div> FAQ </div>
-        </div>
+        <details className={styles.accordion} open>
+          <summary className={styles.subhead}>
+            CUSTOMER CARE <span className={styles.span}>&#x3e;</span>
+          </summary>
+          <div className={styles.life}>
+            <div>Size Guide</div>
+            <div>Shipping and Exchange</div>
+            <div>Store Locator</div>
+            <div>Privacy policy</div>
+            <div>FAQ</div>
+          </div>
+        </details>
       </div>
     </div>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -4,22 +4,6 @@ import styles from "./page.module.css";
 import HomepageCSR from "./components/clientPagesComponents/HomepageCSR";
 import maindata from "./list";
 export default async function Home() {
-  // const filePath = path.join(process.cwd(), "app", "list.json");
-
-  // if (!fs.existsSync(filePath)) {
-  //   console.error(`File not found: ${filePath}`);
-  //   return (
-  //     <main className={styles.main}>
-  //       <p>File not found: {filePath}</p>
-  //     </main>
-  //   );
-  // }
-
-  // const jsonData = fs.readFileSync(filePath, "utf8");
-  // const data = JSON.parse(jsonData);
-
-  // const data = JSON.parse(maindata);
-
   return (
     <>
       <main className={styles.main}>
