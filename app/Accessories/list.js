864d7802521eff3b98cd3df55f2fb80c2d39983f
[Aun Ali] <[rajaaunalikhan@gmail.com]>
2024-08-15 16:59:18 +0500

    Make an Accessories route

---
+++ app/Accessories/list.js
@@ -0,0 +1,105 @@
+const slides = [
+  {
+    src: [
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC05789.jpg%3Fwidth%3D640",
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC05781_bfbc68fa-9412-4570-9ac2-5459bc137c87.jpg%3Fwidth%3D640",
+    ],
+    text: "Mehru KLX-00961 (3 PC)",
+    price: "PKR 8,796",
+  },
+  {
+    src: [
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC06212.jpg%3Fwidth%3D640",
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC06246.jpg%3Fwidth%3D640",
+    ],
+    text: "Mehru KLX-00961 (3 PC)",
+    price: "PKR 8,796",
+  },
+  {
+    src: [
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC06160.jpg%3Fwidth%3D640",
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC06199.jpg%3Fwidth%3D640",
+    ],
+    text: "Mehru KLX-00961 (3 PC)",
+    price: "PKR 8,796",
+  },
+  {
+    src: [
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/dsc06092.jpg%3Fwidth%3D640",
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/dsc06095.jpg%3Fwidth%3D640",
+    ],
+    text: "Mehru KLX-00961 (3 PC)",
+    price: "PKR 8,796",
+  },
+  {
+    src: [
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC05734.jpg%3Fwidth%3D640",
+      "https://kross-kulture.comverseglobal.com/products/klx-00971-3-pc",
+    ],
+    text: "Mehru KLX-00961 (3 PC)",
+    price: "PKR 8,796",
+  },
+  {
+    src: [
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/D71A8866.jpg%3Fwidth%3D640",
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/D71A8862.jpg%3Fwidth%3D640",
+    ],
+    text: "Mehru KLX-00961 (3 PC)",
+    price: "PKR 8,796",
+  },
+  {
+    src: [
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/D71A8755w1.jpg%3Fwidth%3D640",
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/D71A8784w1.jpg%3Fwidth%3D640",
+    ],
+    text: "Mehru KLX-00961 (3 PC)",
+    price: "PKR 8,796",
+  },
+
+  {
+    src: [
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC05665_78efe453-6582-4a89-bbc2-c88dca576c11.jpg%3Fwidth%3D640",
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC05667_f427bf70-e47b-4268-87a5-cdbcdea45c3e.jpg%3Fwidth%3D640",
+    ],
+    text: "Mehru KLX-00961 (3 PC)",
+    price: "PKR 8,796",
+  },
+
+  {
+    src: [
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC05619_47e3c236-4ddc-477d-af4e-c3ce424ef2a1.jpg%3Fwidth%3D640",
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC05644_b03e2933-ce4a-4401-880b-676dd81f8f3e.jpg%3Fwidth%3D640",
+    ],
+    text: "Mehru KLX-00961 (3 PC)",
+    price: "PKR 8,796",
+  },
+
+  {
+    src: [
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC05696_b84b9b64-e187-452e-96ca-64609e43b307.jpg%3Fwidth%3D640",
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC05700_7fec14ad-79dd-4f1e-9d83-e0ef4b02c4cd.jpg%3Fwidth%3D640",
+    ],
+    text: "Mehru KLX-00961 (3 PC)",
+    price: "PKR 8,796",
+  },
+
+  {
+    src: [
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC05512_6a1abd3c-6bef-49c1-89e0-da4f8ccb0fe0.jpg%3Fwidth%3D640",
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC05553_eb82663a-af0e-4581-85cf-7cf95ae2289a.jpg%3Fwidth%3D640",
+    ],
+    text: "Mehru KLX-00961 (3 PC)",
+    price: "PKR 8,796",
+  },
+
+  {
+    src: [
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC05584_8d832411-e1b3-43a9-85b1-e17b4dbfbb6d.jpg%3Fwidth%3D640",
+      "https://cdn.comverseglobal.com/kross_kulture/products/product_images/DSC05589_0fef1835-5207-45c3-a6d2-81c9d3c3e5b0.jpg%3Fwidth%3D640",
+    ],
+    text: "Mehru KLX-00961 (3 PC)",
+    price: "PKR 8,796",
+  },
+];
+
+module.exports = { slides };
--- app/Accessories/page.jsx
+++ app/Accessories/page.jsx
@@ -1,5 +1,17 @@
 import React from "react";
-
+import Card from "./card";
+import { slides } from "./list";
+import styles from "./accessories.module.css";
 export default function Accessories() {
-  return <div>Accessories</div>;
+  return (
+    <div>
+      <div className={styles.card}>
+        {slides.map((s) => (
+          <div>
+            <Card source={s.src} text={s.text} price={s.price}></Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
